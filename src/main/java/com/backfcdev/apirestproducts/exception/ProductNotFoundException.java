371c4c83d024517d301aad631184ea664838f690
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-06-17 11:52:57 -0500

    remove

--- src/main/java/com/backfcdev/apirestproducts/exception/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.backfcdev.apirestproducts.exception;
-
-public class ProductNotFoundException extends RuntimeException{
-    public ProductNotFoundException(long id){
-        super("The product with id "+id+" does not exist");
-    }
-}
