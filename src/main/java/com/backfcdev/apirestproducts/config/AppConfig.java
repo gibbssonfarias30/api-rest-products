7ac653d446530578f03ca542a6a177a7a4bc5bff
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-06-17 22:22:16 -0500

    generation of accurate and complete API documentation in OpenAPI format

--- src/main/java/com/backfcdev/apirestproducts/config/AppConfig.java
+++ src/main/java/com/backfcdev/apirestproducts/config/AppConfig.java
@@ -1,9 +1,31 @@
 package com.backfcdev.apirestproducts.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Gibbsson J. A. Farias Castillo",
+                        email = "gibbfarc17@gmail.com",
+                        url = "https://github.com/gibbssonfarias30"
+                ),
+                description = "OpenAPI documentation for Rest API Products",
+                title = "OpenApi specification - backfcdev",
+                version = "1.0"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:9898"
+                )
+        }
+)
 @Configuration
 public class AppConfig {
     @Bean
