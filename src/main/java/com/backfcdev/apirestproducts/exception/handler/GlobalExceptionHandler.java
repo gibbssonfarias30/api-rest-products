399e4036fc894388bb6b517ee114421711ed0f04
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-06-24 13:01:15 -0500

    creating custom exceptions and being called from Global Handler Exception

--- src/main/java/com/backfcdev/apirestproducts/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/backfcdev/apirestproducts/exception/handler/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.backfcdev.apirestproducts.exception.handler;
 
 
 import com.backfcdev.apirestproducts.exception.EntityNotFoundException;
+import com.backfcdev.apirestproducts.exception.MediaFileNotFoundException;
+import com.backfcdev.apirestproducts.exception.StorageException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,6 +11,10 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.net.URI;
 import java.time.Instant;
+
+import static org.springframework.http.HttpStatus.EXPECTATION_FAILED;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -17,6 +23,24 @@ public class GlobalExceptionHandler {
     ProblemDetail handleEntityNotFoundException(EntityNotFoundException ex){
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, null);
         problemDetail.setTitle("Entity Not Found");
+        problemDetail.setType(URI.create("/not-found"));
+        problemDetail.setProperty("timestamp", Instant.now());
+        return problemDetail;
+    }
+
+    @ExceptionHandler(StorageException.class)
+    ProblemDetail handleStorageException(StorageException ex){
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(EXPECTATION_FAILED, ex.getMessage());
+        problemDetail.setTitle("Expectation Failed");
+        problemDetail.setType(URI.create("/expectation-failed"));
+        problemDetail.setProperty("timestamp", Instant.now());
+        return problemDetail;
+    }
+
+    @ExceptionHandler(MediaFileNotFoundException.class)
+    ProblemDetail handleMediaFileNotFoundException(MediaFileNotFoundException ex){
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(NOT_FOUND, ex.getMessage());
+        problemDetail.setTitle("Not Found");
         problemDetail.setType(URI.create("/not-found"));
         problemDetail.setProperty("timestamp", Instant.now());
         return problemDetail;
